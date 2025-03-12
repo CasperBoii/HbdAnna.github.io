77194719a193985cffc18998905cf5602c6bff77
CasperBoii <127287452+CasperBoii@users.noreply.github.com>
2025-03-12 22:07:24 +0700

    add firework

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Mar 13, 2025 00:00:00").getTime();
+var countDownDate = new Date("Mar 12, 2025 00:00:00").getTime();
 
 var countdownFunction = setInterval(function () {
   var now = new Date().getTime();
