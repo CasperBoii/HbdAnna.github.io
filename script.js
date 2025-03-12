0e9112e9da851505bd7e8fec0eded7a33bbb6777
CasperBoii <127287452+CasperBoii@users.noreply.github.com>
2025-03-12 20:42:52 +0700

    test

---
+++ script.js
@@ -0,0 +1,18 @@
+var countDownDate = new Date("Mar 13, 2025 00:00:00").getTime();
+
+var countdownFunction = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(countdownFunction);
+    window.location.href = "hbd.html";
+  }
+}, 1000);
